2c289cf9b1b707a347c80b5c6953a7f82cff8f21
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-07-23 00:22:43 +0000

    <create> class: Server

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 """simple Pagination."""
-index_range = __import__('0-simple_helper_function').index_range
-import csv
 import math
+import csv
 from typing import List
+index_range = __import__('0-simple_helper_function').index_range
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initialize the class Server."""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cache dataset."""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -26,20 +26,16 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            #assert both args are integers greater than 0.
-            #index range to find the correct indexed to paginate the data set and return the 
-            #appropriate page of the dataset
-            #If inputs are out of range for dataset an empty list should be returned.
+        """Return paginated list."""
+        p = isinstance(page, int) and page > 0
+        p_s = isinstance(page_size, int) and page_size > 0
+        assert p and p_s
+        try:
+            res = index_range(page, page_size)
+            start = res[0]
+            stop = res[1]
+            data_set = self.dataset()
+            return data_set[start:stop]
 
-            p = isinstance(page, int) and page > 0
-            p_s = isinstance(page_size, int) and page_size > 0
-            assert p and p_s
-            try:
-                res = index_range(page, page_size)
-                start = res[0]
-                stop = res[1]
-                data_set  = self.dataset()
-                return data_set[start:stop]
-
-            except IndexError:
-                print([])
+        except IndexError:
+            print([])
