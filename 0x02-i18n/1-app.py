2024567b6eaca573d02988af9b2d90037d32a8db
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-07-30 23:12:43 +0000

    bonjour

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python3
-"""1-app.py."""
-from flask import Flask, render_template
+"""
+Basic Flask app module
+"""
 from flask_babel import Babel
+from flask import Flask, render_template
 
 
 class Config:
-    """Configure class languages."""
-
-    LANGUAGES = ['en', 'fr']
+    """
+    Class representing a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
-
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
+
+
+@app.route('/')
+def get_index() -> str:
+    """
+    home/index page function
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
